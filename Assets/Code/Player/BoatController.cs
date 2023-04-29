f2af3bc8bd279c846251fbfa7cd38c5394b5455a
Damien Ostler <damienostler1@outlook.com>
2023-04-29 13:04:51 -0700

    fixed projecct files

---
+++ Assets/Code/Player/BoatController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BoatController : MonoBehaviour
+{
+    public float Speed = 10f;
+    public float TurnSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
