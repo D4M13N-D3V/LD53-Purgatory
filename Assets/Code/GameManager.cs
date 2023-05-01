ea93e2f0e4187e7e5653bebd56917ddd721fbdad
Damien Ostler <damienostler1@outlook.com>
2023-05-01 06:33:07 -0400

    Update GameManager.cs

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -20,7 +20,6 @@ public class GameManager : MonoBehaviour
     public int CurrencyAmount = 0;
     public int SoulAmount = 0;
     public List<Purgatory.Upgrades.UpgradeSciptableObject> StartingUpgrades = new List<UpgradeSciptableObject>();
-    private PlayerSaveScriptableObject _playerSave;
 
     public static GameManager instance;
     private bool fading = false;
@@ -37,15 +36,6 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    private void Start()
-    {
-
-        if (AssetDatabase.LoadAssetAtPath("Assets/PlayerSave.asset", typeof(PlayerSaveScriptableObject))==null)
-        {
-            AssetDatabase.CreateAsset(new PlayerSaveScriptableObject(), "Assets/PlayerSave.asset");
-        }
-        _playerSave = AssetDatabase.LoadAssetAtPath("Assets/PlayerSave.asset", typeof(PlayerSaveScriptableObject)) as PlayerSaveScriptableObject;
-    }
 
     public void SetGameState(EnumGameState state)
     {
@@ -67,7 +57,6 @@ public class GameManager : MonoBehaviour
         var soulsToRemove = SoulAmount-Mathf.RoundToInt(SoulAmount * SoulCollectionController.instance.SoulRetentionRate);
         SoulCollectionController.instance.RemoveSoul(soulsToRemove);
         UpgradeController.instance.RefreshStats();
-        _playerSave.Upgrades = UpgradeController.instance.Upgrades;
         LoadScene("Death_Shop");
     }
 
@@ -83,12 +72,8 @@ public class GameManager : MonoBehaviour
 
     public void NewGame()
     {
-        _playerSave.Upgrades = StartingUpgrades;
-        _playerSave.Souls = 0;
-        _playerSave.CurrencyAmount = 0;
-        _playerSave.CurrentLevel = 0;
-        UpgradeController.instance.Upgrades = _playerSave.Upgrades;
         LoadScene(IntroductionSceneName);
+        ProjectileHandler.instance.AvailableProjectiles = AvailableProjectiles;
     }
 
     public void NextRun()
